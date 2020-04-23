267f467f9938f6363724b0d076b38d22e97a17f8
nguyenvinhtriet <nguyenvinhtriet@gmail.com>
2020-04-23 10:52:25 +0700

    Create 【04】First Word Simplified.py

---
+++ 【04】First Word Simplified.py
@@ -0,0 +1,24 @@
+# =====================================
+# --*-- coding: utf-8 --*--
+# @Author  : trietnv
+# @Date    : 2020-04-23
+# @CSDN    : 
+# @FileName: 【04】First Word Simplified.py
+# =====================================
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    # your code here
+    return text.split(" ", 1)[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word("a word") == "a"
+    assert first_word("hi") == "hi"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
